daa01827b978cb77bb6d55fd1e4a8f308ccb61f7
brherr <herrbrad@yahoo.com>
2023-09-11 18:16:36 -0400

    intial

---
+++ ClarkCodingChallenge/Models/MailingListEntry.cs
@@ -0,0 +1,9 @@
+namespace ClarkCodingChallenge.Models
+{
+    public class MailingListEntry
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
